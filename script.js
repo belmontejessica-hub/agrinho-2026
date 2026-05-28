c59a90bdcc4e96beef81c9b9b014e9ee5cc2b8c2
belmontejessica-hub <belmonte.jessica@escola.pr.gov.br>
2026-05-28 10:46:59 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function saibaMais() {
+    alert("Bem-vindo à AgroVerde! Estamos preparando conteúdos sobre agricultura regenerativa e tecnologia de precisão.");
+    window.scrollTo({
+        top: document.getElementById('sobre').offsetTop - 70,
+        behavior: 'smooth'
+    });
+}
